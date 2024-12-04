e4f7bc0123ca33dfe555a5f5e631fe62e43b7b47
mrzwk-b <bmerkey@gmail.com>
2024-12-04 13:22:04 -0500

    04 **

---
+++ 04/pt1.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+List<String> getData([String inputFileName = "input.txt"]) {
+  return File([Directory.current.path, inputFileName].join('\\')).readAsLinesSync();
+}
+
+String word = "XMAS";
+
+class Vector {
+  int row;
+  int col;
+  Vector(this.row, this.col);
+  
+  Vector operator +(Vector other) => Vector(row + other.row, col + other.col);
+  Vector operator *(int other) => Vector(row * other, col * other);
+}
+
+void main() {
+  List<String> data = getData();
+  int total = 0;
+
+  int height = data.length;
+  int width = data[0].length;
+  // iterate over each space of the word search (word can start anywhere)
+  for (int row = 0; row < height; row++) {
+    for (int col = 0; col < width; col++) {
+      Vector wordStart = Vector(row, col);
+      // iterate over each direction the word can go in
+      for (Vector direction in [
+        Vector(-1, -1), Vector(-1, 0), Vector(-1, 1),
+        Vector(0, -1),                 Vector(0, 1),
+        Vector(1, -1),  Vector(1, 0),  Vector(1, 1),
+      ]) {
+        bool found = true;
+        // scan direction for the word
+        for (int i = 0; i < 4; i++) {
+          Vector currentSpace = wordStart + (direction * i);
+          if (
+            currentSpace.row < 0 || currentSpace.row >= height ||
+            currentSpace.col < 0 || currentSpace.col >= width ||
+            data[currentSpace.row][currentSpace.col] != word[i]
+          ) {
+            found = false;
+            break;
+          }
+        }
+        if (found) {
+          total += 1;
+        }
+      }
+    }
+  }
+
+  print(total);
+}
---
+++ 04/pt2.dart
@@ -0,0 +1,28 @@
+import 'pt1.dart';
+
+Set<String> corners = {'S', 'M'};
+
+bool mas(String a, String b) => corners.contains(a) && corners.contains(b) && a != b;
+
+void main() {
+  List<String> data = getData();
+  int total = 0;
+
+  int height = data.length;
+  int width = data[0].length;
+  // iterate over basically each space
+  // excluding edges because they can't be the center of an X
+  for (int row = 1; row < height - 1; row++) {
+    for (int col = 1; col < width - 1; col++) {
+      if (
+        data[row][col] == 'A' &&
+        mas(data[row-1][col-1], data[row+1][col+1]) &&
+        mas(data[row-1][col+1], data[row+1][col-1])
+      ) {
+        total += 1;
+      }
+    }
+  }
+
+  print(total);
+}
